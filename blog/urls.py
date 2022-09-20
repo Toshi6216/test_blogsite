00a4c15f8c530c3c483a278d4f81107a42cc2735
toshi <d-toshi@i.softbank.jp>
2022-09-20 14:36:52 +0900

    nabvar mente

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from blog import views
 
 urlpatterns = [
     path('blog/', views.IndexView.as_view(), name='index'),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
+    path('test/', views.BlogTestView.as_view(), name='test'),
 ]
