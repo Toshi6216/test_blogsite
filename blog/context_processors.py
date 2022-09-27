af373e0d9f97d27a51978d76cf491cb9657c6eea
toshi <d-toshi@i.softbank.jp>
2022-09-27 13:22:31 +0900

    sidebar mente

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -2,9 +2,11 @@ from .models import Category, Post
 
 def common(request):
     category_data = Category.objects.all()
+    posts_data = Post.objects.all()
 #    titles = Post.objects.get('title')
     context = {
         'category_data' : category_data,
+        'posts_data' : posts_data,
 #        'titles' : titles
     }
     return context
