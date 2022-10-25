df5894b4db42a9503d2be10d4cac95e007e27565
toshi <d-toshi@i.softbank.jp>
2022-10-25 15:26:24 +0900

    編集権限実装

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -4,10 +4,13 @@ def common(request):
     category_data = Category.objects.all()
 #    posts_data = Post.objects.all()
     posts_data = Post.objects.order_by('-id')
+    for_range = [i for i in range(3)]
 
     context = {
         'category_data' : category_data,
         'posts_data' : posts_data,
-        
+        'for_range': for_range,
     }
+        
+    
     return context
